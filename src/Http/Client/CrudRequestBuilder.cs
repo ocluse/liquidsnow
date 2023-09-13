2d1d7673a3f5e33147ebf79a3da903ddfb57844c
Thismaker <smithkare47@gmail.com>
2023-09-13 05:09:59 +0300

    add crud builder as interfaces

--- src/Http/Client/CrudRequestBuilder.cs
+++ src/Http/Client/CrudRequestBuilder.cs
@@ -3,16 +3,9 @@ using Ocluse.LiquidSnow.Http.Cqrs;
 
 namespace Ocluse.LiquidSnow.Http.Client
 {
-    /// <summary>
-    /// Provides utility methods for making CRUD requests in a RESTful manner
-    /// </summary>
-    /// <typeparam name="TCreate">The request sent to create a resource</typeparam>
-    /// <typeparam name="TUpdate">The request sent to update a resource</typeparam>
-    /// <typeparam name="TList">The request sent as a query to fetch a list of items</typeparam>
-    /// <typeparam name="TModel">The main model type of the resource received from the server</typeparam>
-    /// <typeparam name="TSummary">The model type received from the server for LIST/QUERY operations</typeparam>
-    public class CrudRequestBuilder<TCreate, TUpdate, TList, TModel, TSummary>
-        where TUpdate : IKeyCommand<TModel>
+    ///<inheritdoc cref="ICrudRequestBuilder{TCreate, TUpdate, TList, TModel, TSummary}"/>
+    public class CrudRequestBuilder<TCreate, TUpdate, TList, TModel, TSummary> 
+        : ICrudRequestBuilder<TCreate, TUpdate, TList, TModel, TSummary> where TUpdate : IKeyCommand<TModel>
     {
         private readonly CreateRequestHandler<TCreate, TModel> _createHandler;
         private readonly UpdateRequestHandler<TUpdate, TModel> _updateHandler;
@@ -21,7 +14,7 @@ namespace Ocluse.LiquidSnow.Http.Client
         private readonly ListRequestHandler<TList, TSummary> _listHandler;
 
         /// <summary>
-        /// Creates the request builder to send requests to the specified path in a RESTful manner
+        /// Creates the request builder for the specified REST path.
         /// </summary>
         public CrudRequestBuilder(ISnowHttpClientFactory httpClientFactory, string path, string? clientName = null, IHttpHandler? httpHandler = null)
         {
@@ -32,41 +25,31 @@ namespace Ocluse.LiquidSnow.Http.Client
             _listHandler = new(httpClientFactory, path, clientName, httpHandler);
         }
 
-        /// <summary>
-        /// Sends a request to create a resource
-        /// </summary>
+        ///<inheritdoc/>
         public async Task<TModel> CreateAsync(TCreate create, CancellationToken cancellationToken = default)
         {
             return await _createHandler.ExecuteAsync(create, cancellationToken);
         }
 
-        /// <summary>
-        /// Sends a request to update a resource
-        /// </summary>
+        ///<inheritdoc/>
         public async Task<TModel> UpdateAsync(TUpdate update, CancellationToken cancellationToken = default)
         {
             return await _updateHandler.ExecuteAsync(update, cancellationToken);
         }
 
-        /// <summary>
-        /// Sends a request to read a resource
-        /// </summary>
+        ///<inheritdoc/>
         public async Task<TModel> ReadAsync(string id, CancellationToken cancellationToken = default)
         {
             return await _readHandler.ExecuteAsync(id, cancellationToken);
         }
 
-        /// <summary>
-        /// Sends a request to delete a resource
-        /// </summary>
+        ///<inheritdoc/>
         public async Task DeleteAsync(string id, CancellationToken cancellationToken = default)
         {
             await _deleteHandler.ExecuteAsync(id, cancellationToken);
         }
 
-        /// <summary>
-        /// Send a request to query a list of resources
-        /// </summary>
+        ///<inheritdoc/>
         public async Task<QueryResult<TSummary>> ListAsync(TList? list, CancellationToken cancellationToken = default)
         {
             return await _listHandler.ExecuteAsync(list, cancellationToken);
@@ -74,11 +57,13 @@ namespace Ocluse.LiquidSnow.Http.Client
     }
 
     ///<inheritdoc/>
-    public class CrudRequestBuilder<TCreate, TUpdate, TList, TModel> : CrudRequestBuilder<TCreate, TUpdate, TList, TModel, TModel>
+    public class CrudRequestBuilder<TCreate, TUpdate, TList, TModel> 
+        : CrudRequestBuilder<TCreate, TUpdate, TList, TModel, TModel>,
+        ICrudRequestBuilder<TCreate, TUpdate, TList, TModel>
         where TUpdate : IKeyCommand<TModel>
     {
         /// <summary>
-        /// Creates the request builder to send requests to the specified path in a RESTful manner
+        /// Creates the request builder for the specified REST path.
         /// </summary>
         public CrudRequestBuilder(ISnowHttpClientFactory httpClientFactory, string path, string? clientName = null, IHttpHandler? httpHandler = null) 
             : base(httpClientFactory, path, clientName, httpHandler)
---
+++ src/Http/Client/ICrudRequestBuilder.cs
@@ -0,0 +1,46 @@
+﻿using Ocluse.LiquidSnow.Http.Cqrs;
+
+namespace Ocluse.LiquidSnow.Http.Client
+{
+    /// <summary>
+    /// Provides utility methods for making CRUD requests in a RESTful manner
+    /// </summary>
+    /// <typeparam name="TCreate">The request sent to create a resource</typeparam>
+    /// <typeparam name="TUpdate">The request sent to update a resource</typeparam>
+    /// <typeparam name="TList">The request sent as a query to fetch a list of items</typeparam>
+    /// <typeparam name="TModel">The main model type of the resource received from the server</typeparam>
+    /// <typeparam name="TSummary">The model type received from the server for LIST/QUERY operations</typeparam>
+    public interface ICrudRequestBuilder<TCreate, TUpdate, TList, TModel, TSummary> where TUpdate : IKeyCommand<TModel>
+    {
+        /// <summary>
+        /// Sends a request to create a resource
+        /// </summary>
+        Task<TModel> CreateAsync(TCreate create, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Sends a request to read a resource
+        /// </summary>
+        Task<TModel> ReadAsync(string id, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Sends a request to update a resource
+        /// </summary>
+        Task<TModel> UpdateAsync(TUpdate update, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Sends a request to delete a resource
+        /// </summary>
+        Task DeleteAsync(string id, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Send a request to query a list of resources
+        /// </summary>
+        Task<QueryResult<TSummary>> ListAsync(TList? list, CancellationToken cancellationToken = default);
+    }
+
+    ///<inheritdoc/>
+    public interface ICrudRequestBuilder<TCreate, TUpdate, TList, TModel> : ICrudRequestBuilder<TCreate, TUpdate, TList, TModel, TModel>
+        where TUpdate : IKeyCommand<TModel>
+    {
+    }
+}
