18bfd848d0141302992efc82d0d2c69f952f3819
Thismaker <smithkare47@gmail.com>
2023-09-13 17:08:12 +0300

    add snow client builder

---
+++ src/Http/Client/BuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ocluse.LiquidSnow.Http.Client
+{
+    /// <summary>
+    /// Adds extension methods to types.
+    /// </summary>
+    public static class Extensions
+    {
+        /// <summary>
+        /// Adds the required plumbing to the service collection to use the Snow clients
+        /// </summary>
+        public static ISnowClientBuilder AddSnowClients<T>(this IServiceCollection services)
+             where T : class, ISnowHttpClientFactory
+        {
+            services.AddSingleton<ISnowHttpClientFactory, T>();
+
+            return new SnowClientBuilder(services);
+        }
+    }
+}
---
+++ src/Http/Client/ISnowClientBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Ocluse.LiquidSnow.Http.Client
+{
+    /// <summary>
+    /// A builder for adding Snow clients to the service collection
+    /// </summary>
+    public interface ISnowClientBuilder
+    {
+        /// <summary>
+        /// The service collection where the Snow clients are added
+        /// </summary>
+        IServiceCollection Services { get; }
+    }
+}
