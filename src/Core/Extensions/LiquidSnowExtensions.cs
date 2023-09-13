0b3572f72e671a487c3b28d2c07c46dde23b7a06
Thismaker <smithkare47@gmail.com>
2023-09-13 14:43:44 +0300

    add getmodels extension method

---
+++ src/Core/Extensions/LiquidSnowExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Ocluse.LiquidSnow.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ocluse.LiquidSnow.Extensions
+{
+    /// <summary>
+    /// Adds extension methods to the LiquidSnow library
+    /// </summary>
+    public static class LiquidSnowExtensions
+    {
+        /// <summary>
+        /// Returns a list of entities as their model equivalents
+        /// </summary>
+        /// <remarks>
+        /// If the source is null, an empty list is returned
+        /// </remarks>
+        public static IReadOnlyList<TModel> GetModels<TModel>(this IEnumerable<IEntity<TModel>> entities)
+            where TModel : IModel
+        {
+            return entities?.Select(x => x.GetModel()).ToList() ?? new List<TModel>();
+        }
+    }
+}
