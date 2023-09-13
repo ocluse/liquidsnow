6e73fa23b057a7ace76aa1126bc2a81522ecf077
Thismaker <smithkare47@gmail.com>
2023-09-13 04:02:22 +0300

    add: dated entity

---
+++ src/Core/Entities/IDatedEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ocluse.LiquidSnow.Entities
+{
+    /// <summary>
+    /// An entity with a date and time the entity was created
+    /// </summary>
+    public interface IDatedEntity : IEntity
+    {
+        /// <summary>
+        /// The date and time the entity was created
+        /// </summary>
+        DateTime CreatedAt { get; set; }
+    }
+
+    ///<inheritdoc/>
+    public interface IDatedEntity<T> : IDatedEntity, IEntity<T>
+        where T : IModel
+    {
+    }
+}
