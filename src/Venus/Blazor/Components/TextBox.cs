5e2821585e0f66b801419b9dbebc6d7f067e50e6
Thismaker <smithkare47@gmail.com>
2023-09-17 23:59:00 +0300

    bump venus blazor package version

--- src/Venus/Blazor/Components/TextBox.cs
+++ src/Venus/Blazor/Components/TextBox.cs
@@ -7,16 +7,13 @@ namespace Ocluse.LiquidSnow.Venus.Blazor.Components
     public class TextBox : InputControlBase<string>
     {
         [Parameter]
-        public Keyboard InputType { get; set; }
+        public Keyboard Keyboard { get; set; }
+
+        protected override string InputType => Keyboard.ToString().PascalToKebabCase();
 
         protected override string? GetValue(object? value)
         {
             return value?.ToString() ?? string.Empty;
-        }
-
-        protected override string InputType()
-        {
-            return InputType.ToString().PascalToKebabCase();
         }
     }
 }
