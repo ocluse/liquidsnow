0372a0109aa690eee8da41f313f9dcac41bb754d
Thismaker <smithkare47@gmail.com>
2023-10-07 23:01:13 +0300

    add: accept on upload file

---
+++ src/Venus/Core/Values/InputFileType.cs
@@ -0,0 +1,22 @@
+﻿namespace Ocluse.LiquidSnow.Venus.Values;
+
+/// <summary>
+/// Common input file types
+/// </summary>
+public static class InputFileType
+{
+    /// <summary>
+    /// Accept image files
+    /// </summary>
+    public const string Image = "image/*";
+
+    /// <summary>
+    /// Accept video files
+    /// </summary>
+    public const string Video = "video/*";
+
+    /// <summary>
+    /// Accept audio files
+    /// </summary>
+    public const string Audio = "audio/*";
+}
