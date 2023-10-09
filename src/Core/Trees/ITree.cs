ef1ec418df5348422a946b1d5b184c6639197cf4
Thismaker <smithkare47@gmail.com>
2023-10-09 14:55:56 +0300

    add: trees

---
+++ src/Core/Trees/ITree.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace Ocluse.LiquidSnow.Trees
+{
+    /// <summary>
+    /// A contract for a tree data structure.
+    /// </summary>
+    /// <typeparam name="T">The type of data on each tree node</typeparam>
+    public interface ITree<T>
+    {
+        /// <summary>
+        /// The data contained in the tree node.
+        /// </summary>
+        T? Data { get; }
+
+        /// <summary>
+        /// The parent of the tree node.
+        /// </summary>
+        ITree<T>? Parent { get; }
+
+        /// <summary>
+        /// The children of the tree node.
+        /// </summary>
+        ICollection<ITree<T>> Children { get; }
+        
+        /// <summary>
+        /// Returns true if the tree node is the root of the tree.
+        /// </summary>
+        bool IsRoot { get; }
+
+        /// <summary>
+        /// Returns true if the tree node is a leaf of the tree.
+        /// </summary>
+        bool IsLeaf { get; }
+
+        /// <summary>
+        /// Returns the level of the tree node, with the root node being level 0.
+        /// </summary>
+        int Level { get; }
+    }
+}
