1615ad50238c4bf57c301574fc342b408b28143e
Thismaker <smithkare47@gmail.com>
2023-10-30 17:58:22 +0300

    add: the concept of form container

---
+++ src/Venus/Blazor/Contracts/IFormContainer.cs
@@ -0,0 +1,9 @@
+﻿namespace Ocluse.LiquidSnow.Venus.Blazor.Contracts
+{
+    public interface IFormContainer
+    {
+        void Register(IInput input);
+
+        void Unregister(IInput input);
+    }
+}
