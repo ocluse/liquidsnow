42c336a8e4c8737c70bc647e5146d79601e5e478
Thismaker <smithkare47@gmail.com>
2024-01-08 23:45:24 +0300

    add: queueing of jobs

--- src/Core/Jobs/IJobScheduler.cs
+++ src/Core/Jobs/IJobScheduler.cs
@@ -13,10 +13,25 @@
         /// <summary>
         /// Schedules a job to be run at a particular time.
         /// </summary>
+        /// <remarks>
+        /// Scheduled jobs may be run in parallel if they have the same start time.
+        /// </remarks>
         /// <returns>
         /// Returns an <see cref="IDisposable"/> that can be used to cancel the job.
         /// </returns>
         IDisposable Schedule(IJob job);
+
+        /// <summary>
+        /// Schedules a job to be added to the execution queue at a particular time.
+        /// </summary>
+        /// <param name="job"></param>
+        /// <remarks>
+        /// Queued jobs will always be run one at a time, in the order they were queued, even if they have the same start time.
+        /// </remarks>
+        /// <returns>
+        /// Returns an <see cref="IDisposable"/> that can be used to cancel the job.
+        /// </returns>
+        IDisposable Queue(IJob job);
 
         /// <summary>
         /// Cancels a job with the provided id. If the job has not yet been run, it will be removed from the queue.
