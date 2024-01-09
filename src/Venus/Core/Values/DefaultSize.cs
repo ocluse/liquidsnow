1e994284f40cc37eba2eb6d7f9c4a0f1809a45c1
Thismaker <smithkare47@gmail.com>
2024-01-09 03:04:39 +0300

    add: additional default sizes

--- src/Venus/Core/Values/DefaultSize.cs
+++ src/Venus/Core/Values/DefaultSize.cs
@@ -4,8 +4,12 @@
 
 public static class DefaultSize
 {
+    public const int Size12 = 12;
+    public const int Size14 = 14;
+    public const int Size16 = 16;
     public const int Size18 = 18;
     public const int Size24 = 24;
+    public const int Size32 = 32;
     public const int Size36 = 36;
     public const int Size48 = 48;
 }
