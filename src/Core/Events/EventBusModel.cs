b3b576e8582979adce4108a22ba2a306dba2a02e
Thismaker <smithkare47@gmail.com>
2025-04-20 13:00:24 +0300

    add event bus model to observe unobserved exceptions

---
+++ src/Core/Events/EventBusModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Ocluse.LiquidSnow.Events;
+
+public static class EventBusModel
+{
+    public static event Action<Exception>? UnobservedException;
+
+    internal static void OnUnobservedException(Exception exception)
+    {
+        UnobservedException?.Invoke(exception);
+    }
+}
