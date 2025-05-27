5a96116f7933346f3015a5e89504dce4b3f3078e
Thismaker <smithkare47@gmail.com>
2025-05-28 02:47:30 +0300

    fix: scarping browser extensions

--- src/Scraping/BrowserExtensions.cs
+++ src/Scraping/BrowserExtensions.cs
@@ -4,13 +4,21 @@ using System.Text.Json;
 
 namespace Ocluse.LiquidSnow.Scraping;
 
-internal static class BrowserExtensions
+/// <summary>
+/// Extensions for the <see cref="Browser"/> class to simplify various requests.
+/// </summary>
+public static class BrowserExtensions
 {
+    
+    ///<inheritdoc cref="GetFromJsonAsync{T}(Browser, Uri, RequestOptions, JsonSerializerOptions?, CancellationToken)"/>
     public static async Task<T?> GetFromJsonAsync<T>(this Browser browser, Uri requestUri, RequestOptions requestOptions, CancellationToken cancellationToken = default)
     {
         return await browser.GetFromJsonAsync<T>(requestUri, requestOptions, null, cancellationToken);
     }
 
+    /// <summary>
+    /// Sends a GET request to the specified URI and deserializes the response content into an object of type T.
+    /// </summary>
     public static async Task<T?> GetFromJsonAsync<T>(this Browser browser, Uri requestUri, RequestOptions requestOptions, JsonSerializerOptions? jsonOptions, CancellationToken cancellationToken = default)
     {
         using HttpRequestMessage requestMessage = new(HttpMethod.Get, requestUri);
