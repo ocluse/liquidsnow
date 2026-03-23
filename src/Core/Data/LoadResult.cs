b0bf04a62dd0619492cd7d2a8ebb3112bfea0ad0
Thismaker <smithkare47@gmail.com>
2026-03-24 01:21:29 +0300

    add: empty style load result

--- src/Core/Data/LoadResult.cs
+++ src/Core/Data/LoadResult.cs
@@ -21,4 +21,24 @@ public record LoadResult<TKey, TItem>
     /// The data items loaded for the current page.
     /// </summary>
     public required IReadOnlyList<TItem> Items { get; init; }
+
+    /// <summary>
+    /// Creates a new, empty instance of the <see cref="LoadResult{TKey, TItem}"/> class with no items and default key values.
+    /// </summary>
+    /// <remarks>
+    /// Use this method to represent a result with no data, such as when a data source returns no
+    /// items for a given query.
+    /// </remarks>
+    /// <returns>An empty <see cref="LoadResult{TKey, TItem}"/> instance with the Items collection empty and both NextKey and PreviousKey set
+    /// to their default values.
+    /// </returns>
+    public static LoadResult<TKey, TItem> Empty()
+    {
+        return new LoadResult<TKey, TItem>
+        {
+            NextKey = default,
+            PreviousKey = default,
+            Items = []
+        };
+    }
 }
